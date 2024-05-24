d47acfe33646c2a5f8e925ac6c1be43ce7677724
Chandrakant <cks952526@gmail.com>
2024-05-24 21:27:08 +0530

    Add url

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,26 +1,36 @@
-import {format} from "date-fns";
-import {Link} from "react-router-dom";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Post({_id,title,summary,cover,content,createdAt,author}) {
-    return (
-        <div className="post">
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
+  return (
+    <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-        <img src={'http://localhost:4000/'+cover} alt="" />
-        </Link> 
-      </div> 
+          <img src={"https://blog-app-icb6.onrender.com/" + cover} alt="" />
+        </Link>
+      </div>
 
       <div className="texts">
         <Link to={`/post/${_id}`}>
-        <h2>{title}</h2>
+          <h2>{title}</h2>
         </Link>
-       
-       <p className="info">
-          <a href="https://google.com" className="author">{author.username}</a>
-          <time>{format(new Date(createdAt), '  MMM d,yyy HH:mm')}</time>
-       </p>
-       <p className="summary">{summary}</p>
-       </div>      
+
+        <p className="info">
+          <a href="https://google.com" className="author">
+            {author.username}
+          </a>
+          <time>{format(new Date(createdAt), "  MMM d,yyy HH:mm")}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
     </div>
-    )
+  );
 }
--- client/src/pages/createPost.js
+++ client/src/pages/createPost.js
@@ -1,56 +1,58 @@
-import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import { Navigate } from 'react-router-dom';
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import Editor from "../Editor.js";
- 
-  
+
 export default function CreatePost(ev) {
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
-  const [files,setFiles] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
-  
-  async function createNewPost(ev){
+
+  async function createNewPost(ev) {
     const data = new FormData();
-    data.set('title',title);
-    data.set('summary',summary);
-    data.set('content',content);
-    data.set('file',files[0]);
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
     ev.preventDefault();
-    
-    const response = await fetch('http://localhost:4000/post' ,{
-      method: 'POST',
+
+    const response = await fetch("https://blog-app-icb6.onrender.com/post", {
+      method: "POST",
       body: data,
-      credentials:'include',
+      credentials: "include",
     });
-    
-    if(response.ok){
+
+    if (response.ok) {
       setRedirect(true);
     }
   }
 
-  if(redirect){
-    return <Navigate to={'/'} />
+  if (redirect) {
+    return <Navigate to={"/"} />;
   }
 
-    return(
-        <form onSubmit={createNewPost}>
-            <input type="title" 
-                placeholder={'Title'}  
-                value={title}  
-                onChange={ev => setTitle(ev.target.value)}/>
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
 
-            <input type="summary" 
-                placeholder={'summary'}
-                value={summary}
-                onChange={ev => setSummary(ev.target.value)}/>
+      <input
+        type="summary"
+        placeholder={"summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
 
-            <input type="file" 
-                onChange={ev => setFiles(ev.target.files)}/>
-               
-               <Editor value={content} onChange={setContent}/>
-            <button style={{marginTop:'10px'}}>Create Post</button>
-        </form>
-    );
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+
+      <Editor value={content} onChange={setContent} />
+      <button style={{ marginTop: "10px" }}>Create Post</button>
+    </form>
+  );
 }
